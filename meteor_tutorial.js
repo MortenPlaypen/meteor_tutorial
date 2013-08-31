638604a4ab78468eacf18892aec47675ce9e0770
MortenPlaypen <morten@playpenlabs.com>
2013-08-31 11:52:41 -0700

    Initial commit

---
+++ meteor_tutorial.js
@@ -0,0 +1,26 @@
+var Products = new Meteor.Collection("Products");
+
+if (Meteor.isClient) {
+
+Template.Products.ProductArr = function(){
+   return Products.find({}, {sort: {Name: 1}});
+};
+
+
+Template.Products.events = {
+   "click .Product" : function(){
+      if(this.InStock)
+         confirm("Would you like to buy a " + this.Name + " for " + this.Price + "$");
+      else
+         alert("That item is not in stock");
+   }
+ }
+
+
+};
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
